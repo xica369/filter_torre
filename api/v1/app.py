7f163dc73fb611ff8d890c45ba02833d8de66391
xica369 <761@holbertonschool.com>
2019-11-18 01:07:27 +0000

    Base structure to API backend

---
+++ api/v1/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application:
+web application must be listening on 0.0.0.0, port 5000
+"""
+
+from flask import Flask, jsonify
+from api.v1.views import app_views
+from os import getenv
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+
+@app.errorhandler(404)
+def errot_notfound(message):
+    """ handler for 404 errors """
+    resp = jsonify({"error": "Not found"})
+    resp.status_code = 404
+    return resp
+
+if __name__ == '__main__':
+    app.run(port=int(getenv("API_PORT")),
+            host=getenv("API_HOST"), threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+import app_views and create a route /status
+"""
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
+def status_route():
+    """returns a JSON: "status": "OK" """
+    return jsonify({"status": "OK"})
