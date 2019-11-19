74d1c8a43d75a18f787873f0364017ab97e525c8
xica369 <761@holbertonschool.com>
2019-11-19 07:32:18 +0000

    [feature]: flask-cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,10 +7,15 @@ web application must be listening on 0.0.0.0, port 5000
 from flask import Flask, jsonify
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+
+# Cross-Origin Resource Sharing
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 @app.errorhandler(404)
 def errot_notfound(message):
